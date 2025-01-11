25a06f3410efcfcd6031371ca2df059ff48bb21b
Syed Fahad Abbas <110738135+fahadabbas817@users.noreply.github.com>
2025-01-11 18:06:56 +0500

    Real estate assistant integrated with the chat_assistant api

---
+++ frontend/src/components/ChatMessage.jsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
+import user from "@/assets/user.png";
+import bot from "@/assets/bot.png";
+
+export default function ChatMessage({ message, isUser, isLoading }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: isUser ? 100 : -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.3, ease: "easeOut" }}
+      className={`flex gap-3 ${
+        isUser ? "flex-row-reverse" : "flex-row"
+      } items-start mb-4`}
+    >
+      <Avatar className="w-8 h-8 md:w-12 md:h-12 bg-cyan-400">
+        <AvatarImage src={isUser ? user : bot} />
+        <AvatarFallback className="bg-cyan-500">
+          {isUser ? "U" : "B"}
+        </AvatarFallback>
+      </Avatar>
+
+      {isLoading ? (
+         <div className="space-y-2">
+         <Skeleton className="h-4 w-56 rounded-full bg-white" />
+         <Skeleton className="h-4 w-80 rounded-full bg-white" />
+       </div>
+      ) : (
+        <motion.div
+          initial={{ scale: 0.95 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.2 }}
+          className={`max-w-[80%] rounded-2xl p-4 ${
+            isUser
+              ? "bg-cyan-600 text-white"
+              : "bg-slate-700/50 backdrop-blur-xl text-white"
+          }`}
+        >
+          {message}
+        </motion.div>
+      )}
+    </motion.div>
+  );
+}
---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { motion } from 'framer-motion';
+
+const HeroSection = () => {
+
+  return (
+
+    <motion.div
+    initial={{ opacity: 0, y: -20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 1, ease: "easeOut" }}
+    className="text-center mt-24 mb-12"
+  >
+    {/* <h1 className='text-4xl font-extrabold text-white md:text-5xl'>Real Estate AI Assistant</h1> */}
+    <h1 className=" font-heading font text-4xl font-bold text-white md:text-5xl ">
+      Find Your{" "}
+      <span className="bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent font-extrabold animate-pulse">Dream Property</span>{" "}
+     
+      with Ease!
+    </h1>
+    <p className="mt-4 text-lg text-slate-300 md:text-xl">
+      Let our{" "} Real Estate
+        AI Assistant
+   
+      guide you to your perfect space.
+    </p>
+  </motion.div>
+
+  )
+}
+
+export default HeroSection
